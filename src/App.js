cdf21b209662ee374b583401e5035af5ea533591
carlos arturo chacon osorio <carlos.chacon@gexnova.com.co>
2022-04-26 15:59:30 -0500

    Creando PhotoCard y usando react-icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { ListOfCategory } from './components/ListOfCategory'
+import { ListOfPhotoCard } from './components/ListOfPhotoCard'
 import { GlobalStyles } from './GlobalStyles'
 
 export const App = () => (
   <div>
     <GlobalStyles />
     <ListOfCategory />
+    <ListOfPhotoCard />
   </div>
 )
