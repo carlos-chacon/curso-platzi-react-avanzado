3d8a7dc76562569dc569974922aa5fab4281d29d
carlos arturo chacon osorio <carlos.chacon@gexnova.com.co>
2022-04-26 17:34:17 -0500

    ¿Qué son los Hooks?

--- src/components/ListOfCategory/index.js
+++ src/components/ListOfCategory/index.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { Item, List } from './styles'
-import db from '../../../api/db.json'
+// import db from '../../../api/db.json'
 
 export const ListOfCategory = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    window.fetch('https://petgram-server-carlos-chacon.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
   return (
     <List>
       {
-        db.categories.map(category => <Item key={category.id}> <Category {...category} /> </Item>)
+        categories.map(category => <Item key={category.id}> <Category {...category} /> </Item>)
       }
     </List>
   )
