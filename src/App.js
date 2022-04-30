2bb17abc3be39473e78c24a2d1cf7a16c81374ff
carlos arturo chacon osorio <carlos.chacon@gexnova.com.co>
2022-04-30 11:49:40 -0500

    Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+/* eslint-disable react/jsx-closing-tag-location */
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
 import { Home } from './pages/Home'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { User } from './pages/User'
 import { GlobalStyles } from './styles/GlobalStyles'
 
 export const App = () => {
+  const UserLogged = ({ children }) => {
+    return children({ isAuth: false })
+  }
   return (
     <div>
       <GlobalStyles />
@@ -16,6 +23,20 @@ export const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/pet/:id' element={<Home />} />
       </Routes>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Routes>
+                <Route path='/favs' element={<Favs />} />
+                <Route path='/user' element={<User />} />
+              </Routes>
+              : <Routes>
+                <Route path='/favs' element={<NotRegisteredUser />} />
+                <Route path='/user' element={<NotRegisteredUser />} />
+              </Routes>
+        }
+      </UserLogged>
       <NavBar />
     </div>
   )
