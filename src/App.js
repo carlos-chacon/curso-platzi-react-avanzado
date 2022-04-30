4f75b8983befda6b8af913c80b44506fea14f94c
carlos arturo chacon osorio <carlos.chacon@gexnova.com.co>
2022-04-30 11:00:31 -0500

    Agregando un NavBar a nuestra app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Logo } from './components/Logo'
+import { NavBar } from './components/NavBar'
 import { Detail } from './pages/Detail'
 import { Home } from './pages/Home'
 import { GlobalStyles } from './styles/GlobalStyles'
@@ -15,6 +16,7 @@ export const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/pet/:id' element={<Home />} />
       </Routes>
+      <NavBar />
     </div>
   )
 }
