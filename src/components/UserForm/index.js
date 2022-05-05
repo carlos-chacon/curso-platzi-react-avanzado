b14a5a353eb612bedfed163d3e96dd36935ecaf6
carlos arturo chacon osorio <carlos.chacon@gexnova.com.co>
2022-05-05 08:47:03 -0500

    Controlar estado de carga y error al registrar un usuario

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useInputValue } from '../../hooks/useInputValue'
-import { Button, Form, Input, Title } from './style'
+import { Button, Error, Form, Input, Title } from './style'
 
-export const UserForm = ({ onSubmit, title }) => {
+export const UserForm = ({ onSubmit, title, error, disabled }) => {
   const email = useInputValue('')
   const password = useInputValue('')
   const handleSubmit = (event) => {
@@ -11,11 +11,12 @@ export const UserForm = ({ onSubmit, title }) => {
   }
   return (
     <>
-      <Title>{title}</Title>
-      <Form onSubmit={handleSubmit}>
-        <Input placeholder='Email' {...email} />
-        <Input placeholder='Password' type='password' {...password} />
-        <Button>{title}</Button>
+      <Form disabled={disabled} onSubmit={handleSubmit}>
+        <Title>{title}</Title>
+        <Input disabled={disabled} placeholder='Email' {...email} />
+        <Input disabled={disabled} placeholder='Password' type='password' {...password} />
+        <Button disabled={disabled}>{title}</Button>
+        {error && <Error>{error}</Error>}
       </Form>
     </>
   )
