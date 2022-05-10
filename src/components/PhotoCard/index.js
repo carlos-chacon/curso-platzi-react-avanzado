e673e4035dbccdb8ffb83940944f47eb4e1defce
carlos arturo chacon osorio <carlos.chacon@gexnova.com.co>
2022-05-10 16:57:20 -0500

    Hacer like como usuario registrado

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useLocalStore } from '../../hooks/useLocalStorage'
 import { useMutationToggleLike } from '../../hooks/useMutationToggleLike'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton'
@@ -8,19 +7,16 @@ import { Article, Img, ImgWrapper } from './styles'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStore(key, false)
+export const PhotoCard = ({ id, likes = 0, liked, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
   const { mutation } = useMutationToggleLike()
 
   const handleFavClick = () => {
-    !liked && mutation({
+    mutation({
       variables: {
         input: { id }
       }
     })
-    setLiked(!liked)
   }
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache, from } from '@apollo/client'
 // import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client'
+import { setContext } from '@apollo/client/link/context'
+import { onError } from '@apollo/client/link/error'
 import { App } from './App'
 import Context from './Context'
 
+const httpLink = createHttpLink({
+  uri: 'https://petgram-server-carlos-chacon.vercel.app/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = window.sessionStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) => {
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+      window.sessionStorage.removeItem('token')
+      // window.location.href = '/user'
+    })
+  }
+  if (networkError && networkError.result.code === 'invalid_token') {
+    window.sessionStorage.removeItem('token')
+    window.location.href = '/'
+  }
+})
+
 const client = new ApolloClient({
-  uri: 'https://petgram-server-carlos-chacon.vercel.app/graphql',
+  link: from([errorLink, authLink.concat(httpLink)]),
   cache: new InMemoryCache()
 })
 
